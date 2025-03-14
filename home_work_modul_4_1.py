6d02f0bcbe5eaebabd7aa4564946f14cc8f3416a
maksymfedunov <maksim.kherson88@gmail.com>
2025-03-14 20:32:07 +0100

    first_commit

---
+++ home_work_modul_4_1.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+
+path = Path("salary_file.txt")
+
+def total_salary(path):
+    
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            salary = list()
+            for line in file:
+                salary.append (float(line.split(",")[1]))
+            total = sum(salary)
+            average = total / len(salary)
+            total_salary = (total, average)
+        return total_salary    
+    except Exception as e:
+        print(f"{e} with file")       
+                
+                    
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
+                
+            
+    
